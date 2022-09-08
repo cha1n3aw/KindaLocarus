f5b55d7fff21ca9c62df21f9ed4591dd38e675d1
cha1n3aw <ihatesusu@ya.ru>
2022-09-08 11:30:20 +0500

    Added some device imei and timestamp checks

--- src/main/java/com/dst/smd207api/Controllers/DeviceRawDataController.java
+++ src/main/java/com/dst/smd207api/Controllers/DeviceRawDataController.java
@@ -56,14 +56,25 @@ public class DeviceRawDataController
     public ResponseEntity<Response<?>> devicesGetPos(
             @RequestParam(required = true, name="imeis", defaultValue = "") List<String> imeis,
             @RequestParam(required = false, name="mode", defaultValue = "short") String mode,
-            @RequestParam(required = false, name="from") Instant fromTime,
-            @RequestParam(required = false, name="to") Instant toTime)
+            @RequestParam(required = false, name="from") String objectFromTime,
+            @RequestParam(required = false, name="to") String objectToTime)
     {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken) && authentication.getAuthorities() != null)
         {
-            if (imeis == null || imeis.stream().count() == 0) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Correct IMEIs are required");}}, HttpStatus.OK);
-            if (!mode.equals("full") && !mode.equals("short")) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Correct mode is required");}}, HttpStatus.OK);
+            Instant fromTime;
+            Instant toTime;
+            try
+            {
+                fromTime = Instant.parse(objectFromTime);
+                toTime = Instant.parse(objectToTime);
+            }
+            catch(Exception e)
+            {
+                return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect timestamps: format should be YYYY-MM-DDT00:00:00Z");}}, HttpStatus.OK);
+            }
+            if (imeis == null || imeis.stream().count() == 0) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect IMEIs: at least one correct IMEI required");}}, HttpStatus.OK);
+            if (!mode.equals("full") && !mode.equals("short")) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect mode: should be 'short' or 'full'");}}, HttpStatus.OK);
             if (fromTime != null && toTime != null && fromTime.isAfter(toTime)) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect timestamps: 'From' should precede 'to'");}}, HttpStatus.OK);;
             List<Long> devices = new ArrayList<>();
             if (authentication.getAuthorities().stream().anyMatch(c -> c.getAuthority().equals("ADMIN")))
@@ -73,7 +84,14 @@ public class DeviceRawDataController
                 if (!imeis.contains("all"))
                 {
                     List<Long> longImeis = new ArrayList<>();
-                    for (String dev : imeis) longImeis.add(Long.parseLong(dev));
+                    for (String dev : imeis)
+                        try
+                        {
+                            long tempDev = Long.parseLong(dev);
+                            if (tempDev < 100000000000000L || tempDev > 999999999999999L) throw new Exception();
+                            else longImeis.add(tempDev);
+                        }
+                        catch (Exception e) { }
                     devices.retainAll(longImeis);
                 }
             }
@@ -83,11 +101,18 @@ public class DeviceRawDataController
                 if (!imeis.contains("all"))
                 {
                     List<Long> longImeis = new ArrayList<>();
-                    for (String dev : imeis) longImeis.add(Long.parseLong(dev));
+                    for (String dev : imeis)
+                        try
+                        {
+                            long tempDev = Long.parseLong(dev);
+                            if (tempDev < 100000000000000L || tempDev > 999999999999999L) throw new Exception();
+                            else longImeis.add(tempDev);
+                        }
+                        catch(Exception e) { }
                     devices.retainAll(longImeis);
                 }
             }
-            if (devices == null || devices.stream().count() == 0 ) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Correct IMEIs are required");}}, HttpStatus.OK);
+            if (devices == null || devices.stream().count() == 0 ) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect IMEIs: at least one correct IMEI required");}}, HttpStatus.OK);
             else return new ResponseEntity<>(deviceRawDataService.devicesGetPos(devices, mode, fromTime, toTime), HttpStatus.OK);
         }
         else return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.UNAUTHORIZED.value()); setResponseErrorDesc("Unauthorized");}}, HttpStatus.OK);
@@ -96,16 +121,36 @@ public class DeviceRawDataController
     @GetMapping("/devices.getTrack")
     @ResponseBody
     public ResponseEntity<Response<?>> devicesGetTrack(
-            @RequestParam(required = true, name="imei", defaultValue = "") Long imei,
+            @RequestParam(required = true, name="imei", defaultValue = "") String objectImei,
             @RequestParam(required = false, name="mode", defaultValue = "short") String mode,
-            @RequestParam(required = false, name="from") Instant fromTime,
-            @RequestParam(required = false, name="to") Instant toTime)
+            @RequestParam(required = false, name="from") String objectFromTime,
+            @RequestParam(required = false, name="to") String objectToTime)
     {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken) && authentication.getAuthorities() != null)
         {
-            if (imei == null || imei < 100000000000000L || imei > 999999999999999L) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Correct IMEIs are required");}}, HttpStatus.OK);
-            if (!mode.equals("full") && !mode.equals("short")) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Correct mode is required");}}, HttpStatus.OK);
+            Instant fromTime;
+            Instant toTime;
+            Long imei;
+            try
+            {
+                fromTime = Instant.parse(objectFromTime);
+                toTime = Instant.parse(objectToTime);
+            }
+            catch(Exception e)
+            {
+                return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect timestamps: format should be YYYY-MM-DDT00:00:00Z");}}, HttpStatus.OK);
+            }
+            try
+            {
+                imei = Long.parseLong(objectImei);
+            }
+            catch (Exception e)
+            {
+                return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect IMEI: no characters allowed");}}, HttpStatus.OK);
+            }
+            if (imei < 100000000000000L || imei > 999999999999999L) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect IMEI: should be 15 digits long");}}, HttpStatus.OK);
+            if (!mode.equals("full") && !mode.equals("short")) return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect mode: should be 'short' or 'full'");}}, HttpStatus.OK);
             if (fromTime != null && toTime != null && toTime.isBefore(fromTime))
                 return new ResponseEntity<>(new Response<>(){{setResponseStatus(HttpStatus.BAD_REQUEST.value()); setResponseErrorDesc("Incorrect timestamps: 'From' should precede 'To'");}}, HttpStatus.OK);
             if (authentication.getAuthorities().stream().anyMatch(c -> c.getAuthority().equals("ADMIN")) ||
